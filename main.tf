351cc74004914e09a6f5b7a5a428209a605ed515
Nitika Thingalaya <110655416+NitikaThingalaya-pki@users.noreply.github.com>
2024-06-28 14:14:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "codepipeline-us-east-1-611786723258"
+    bucket         = "codepipeline-us-east-1-611786723258/Terraform_Create_Ins"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
